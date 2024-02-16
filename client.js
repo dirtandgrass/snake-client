6f20aba7d863ee5eec02701f8a81f9499ea9997e
Luke <verdant.luke@gmail.com>
2024-02-16 13:15:39 -0500

    input.js: module to handle user input, not sure why setupInput took the connection object, decoupled it

--- client.js
+++ client.js
@@ -29,20 +29,6 @@ const connect = () => {
 
 
 
-const setupInput = conn => {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
 
-const handleUserInput = (key) => {
-  // check for process exit request (ctrl+c)
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
 
-module.exports = { connect, setupInput };
+module.exports = { connect };
---
+++ input.js
@@ -0,0 +1,17 @@
+const setupInput = () => {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = (key) => {
+  // check for process exit request (ctrl+c)
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+module.exports = { setupInput };
